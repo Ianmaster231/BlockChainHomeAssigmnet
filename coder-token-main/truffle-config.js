951110f8b484e9dbf35c0577fee0787a2af04320
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-01-26 14:16:46 +0100

    Workingon rinkbey server host

--- coder-token-main/truffle-config.js
+++ coder-token-main/truffle-config.js
@@ -1,3 +1,12 @@
+ require('dotenv').config()
+
+ const anemonic = process.env.MVEMONIC_PASSWORD;
+ const RinkebyEndPoint = process.env.RinkebyEndPoint;
+ const RinkbyWSEndpoint = process.env.RINKEBY_WS_END_POINT;
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+ const path = require("path")
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -7,6 +16,14 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     }
+  },
+  rinkeby:{
+    provider:()=>{
+      return new HDWalletProvider(anemonic, RinkbyWSEndpoint);
+    },
+    network_id: 4,
+    gas:4500000,
+    gasPrice: 1000000000,
   },
   compilers: {
     solc: {
